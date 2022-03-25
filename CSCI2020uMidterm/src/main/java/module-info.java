dbfaf54afea919c9541f7745740aac378993f307
Jackie Jiang <jack@LAPTOP-SFPAP0S8.localdomain>
2022-03-25 15:04:12 -0400

    Changed Files

--- CSCI2020uMidterm/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module example.csci2020umidterm {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens example.csci2020umidterm to javafx.fxml;
-    exports example.csci2020umidterm;
-}
